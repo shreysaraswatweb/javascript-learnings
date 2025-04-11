c260dd6d5510f4cfc281ae1367d0868c6c7977b9
shreysaraswatweb <shreysaraswat1996@gmail.com>
2025-04-11 18:00:05 +0530

    Module Pattern-IIFE

--- designPattern.js
+++ designPattern.js
@@ -1,1 +1,32 @@
-console.log("Design Patterns in JavaScript");
+/*Module Patterns*/
+//Initial Invoked Function Expression (IIFE):- Everything inside this function is private
+
+// let bankBalance = 12000; //private variable for using it or changing it.
+let Bank = (function () {
+  let bankBalance = 12000; //private variable for using it or changing it.
+
+  //These functions are in IIFE so we cannot use them directly from outside
+  function checkBalance() {
+    console.log(bankBalance);
+  }
+  function setBalance(val) {
+    bankBalance = val;
+  }
+  function withdraw(val) {
+    if (val <= bankBalance) {
+      bankBalance -= val;
+      console.log(
+        `Your withdraw is successful. Your new balance is ${bankBalance}`
+      );
+    }
+  }
+
+  //Returning the functions which we want to use outside of IIFE
+  return {
+    checkBalance,
+    setBalance,
+    withdraw,
+  };
+})();
+
+Bank.withdraw(11000);
